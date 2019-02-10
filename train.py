43c41f95dd3e41be28dbc3831eea19998c49f11a
Vladimir <10669582+Wovchena@users.noreply.github.com>
2019-02-10 21:04:29 +0300

    update train

--- train.py
+++ train.py
@@ -13,7 +13,7 @@ import tqdm
 def restore_checkpoint(folder):
     model = FOTSModel().to(torch.device("cuda"))
     optimizer = torch.optim.Adam(model.parameters(), lr=0.0001, weight_decay=1e-5)
-    lr_scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, 0.94)
+    lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=1, verbose=True, threshold=0.0001, threshold_mode='rel', cooldown=1, min_lr=0, eps=1e-08)
 
     if os.path.isfile(os.path.join(folder, 'last_checkpoint.pt')):
         checkpoint = torch.load(os.path.join(folder, 'last_checkpoint.pt'))
@@ -94,46 +94,62 @@ def loss_batch(model, loss_func, xb, yb, opt=None):
     return loss.item(), len(xb)
 
 
-def fit(start_epoch, model, loss_func, opt, lr_scheduler, best_score, checkpoint_dir, train_dl, valid_dl):
+def fit(start_epoch, model, loss_func, opt, lr_scheduler, best_score, max_batches_per_iter_cnt, checkpoint_dir, train_dl, valid_dl):
+    batch_per_iter_cnt = 0
     for epoch in range(start_epoch, 9999999):
         model.train()
-        train_loss = 0.0
-        loss_count = 0
-        # pbar = tqdm.tqdm(train_dl, 'Epoch ' + str(epoch), ncols=79)
-        for cropped, classification, regression, thetas, training_mask, file_names in train_dl:
-            loss, count = loss_batch(model, loss_func, cropped, (classification, regression, thetas, training_mask, file_names), opt)
-            train_loss += loss
-            loss_count += count
-            # pbar.set_postfix_str({'Loss': train_loss / loss_count}, refresh=False)
-        lr_scheduler.step(epoch)
-        print('new learning rate: ', lr_scheduler.get_lr()[0])
+        train_loss_stats = 0.0
+        loss_count_stats = 0
+        pbar = tqdm.tqdm(train_dl, 'Epoch ' + str(epoch), ncols=120)
+        for cropped, classification, regression, thetas, training_mask, file_names in pbar:
+            if batch_per_iter_cnt == 0:
+                optimizer.zero_grad()
+            prediction = model(cropped.to('cuda'))
+            loss = loss_func(prediction, (classification, regression, thetas, training_mask, file_names))
+            train_loss_stats += loss.item()
+            loss_count_stats += len(cropped)
+            loss /= max_batches_per_iter_cnt
+            loss.backward()
+            batch_per_iter_cnt += 1
+            if batch_per_iter_cnt == max_batches_per_iter_cnt:
+                opt.step()
+                batch_per_iter_cnt = 0
+                pbar.set_postfix({'Mean loss over the epoch': train_loss_stats / loss_count_stats}, refresh=False)
+        lr_scheduler.step(train_loss_stats / loss_count_stats, epoch)
 
         if valid_dl is None:
-            val_loss = train_loss / loss_count
+            val_loss = train_loss_stats / loss_count_stats
         else:
             model.eval()
             with torch.no_grad():
-                losses, nums = zip(
-                    *[loss_batch(model, loss_func, xb, yb, None) for xb, yb in valid_dl]
-                )
-            val_loss = np.sum(np.multiply(losses, nums)) / np.sum(nums)
-        print(val_loss)
+                val_loss = 0.0
+                val_loss_count = 0
+                for cropped, classification, regression, thetas, training_mask, file_names in valid_dl:
+                    prediction = model(cropped.to('cuda'))
+                    loss = loss_func(prediction, (classification, regression, thetas, training_mask, file_names))
+                    val_loss += loss.item()
+                    val_loss_count += len(cropped)
+            val_loss /= val_loss_count
+        print('Val loss: ', val_loss)
 
-        if best_score < val_loss:
+        if best_score > val_loss:
             best_score = val_loss
             save_as_best = True
         else:
             save_as_best = False
-        # save_checkpoint(epoch, model, opt, lr_scheduler, best_score, checkpoint_dir, save_as_best)
+        save_checkpoint(epoch, model, opt, lr_scheduler, best_score, checkpoint_dir, save_as_best)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--train-folder', type=str, required=True, help='Path to folder with train images and labels')
+    parser.add_argument('--batch-size', type=int, default=8, help='Number of batches to process before train step')
+    parser.add_argument('--batches-before-train', type=int, default=4, help='Number of batches to process before train step')
+    parser.add_argument('--num-workers', type=int, default=8, help='Path to folder with train images and labels')
     args = parser.parse_args()
 
     icdar = datasets.ICDAR2015(args.train_folder, True, datasets.transform)
-    dl = torch.utils.data.DataLoader(icdar, batch_size=8, shuffle=True, sampler=None, batch_sampler=None, num_workers=8)
+    dl = torch.utils.data.DataLoader(icdar, batch_size=args.batch_size, shuffle=True, sampler=None, batch_sampler=None, num_workers=args.num_workers)
     checkoint_dir = 'runs'
     epoch, model, optimizer, lr_scheduler, best_score = restore_checkpoint(checkoint_dir)
-    fit(epoch, model, detection_loss, optimizer, lr_scheduler, best_score, checkoint_dir, dl, None)
+    fit(epoch, model, detection_loss, optimizer, lr_scheduler, best_score, args.batches_before_train, checkoint_dir, dl, None)
